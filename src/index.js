42b7480c85541e0194562ab6f54eda3ca8629891
hanqpark <official03x@gmail.com>
2021-05-27 10:55:55 +0900

    #3.2 GET requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
