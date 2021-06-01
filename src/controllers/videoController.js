7781a7312f4fc5251e0b401e1a961e77a3916058
hanqpark <official03x@gmail.com>
2021-06-01 11:50:27 +0900

    Chapter 5 finished

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,37 @@
 const fakeUser = {
   username: "hanqpark",
-  loggedIn: true,
+  loggedIn: false,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 min ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos, fakeUser });
+};
 export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
