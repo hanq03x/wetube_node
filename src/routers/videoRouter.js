d829aaaf496c49aa29a26a55cff1087a0b109d4e
hanqpark <official03x@gmail.com>
2021-06-07 10:27:25 +0900

    end #6

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,7 +5,7 @@ import {
   getEdit,
   getUpload,
   postUpload,
-  deleteVid,
+  getDelete,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -14,6 +14,6 @@ const videoRouter = express.Router();
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload); //upload를 id로 인식할 수 있음.. => 정규표현식으로 해결!!!
-videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVid);
+videoRouter.get("/:id([0-9a-f]{24})/delete", getDelete);
 
 export default videoRouter;
