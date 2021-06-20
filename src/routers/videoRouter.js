2d9c83a7542bc0220ac35c6e7605f3f45f4583cb
hanqpark <official03x@gmail.com>
2021-06-20 12:21:58 +0900

    #8.2

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,22 @@ import {
   postUpload,
   getDelete,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // 순서가 제일 중요합니다!!
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload); //upload를 id로 인식할 수 있음.. => 정규표현식으로 해결!!!
-videoRouter.get("/:id([0-9a-f]{24})/delete", getDelete);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload); //upload를 id로 인식할 수 있음.. => 정규표현식으로 해결!!!
+videoRouter.route("/:id([0-9a-f]{24})/delete", protectorMiddleware, getDelete);
 
 export default videoRouter;
